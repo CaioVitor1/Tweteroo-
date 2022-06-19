1d13c3520898bf4848da401af6cd278a6e52f766
CaioVitor1 <caiovitor33@hotmail.com>
2022-06-19 17:02:21 -0300

    finalizing requirements

--- index.js
+++ index.js
@@ -4,32 +4,44 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json());
+const everyUser = [];
+const tweets = [];
 
-const username = ""
-const picture = ""
-let contents = ""
-const everyUsers = [];
-const tweets = [{
-	username: "bobesponja",
-        tweet: "eu amo o hub"
-}];
-
-
-
-const message = {
-	username,
-        tweet: contents,
+const lastTweets = function() {
+        for(let i = 0; i < tweets.length; i++) {
+                if(tweets.length < 10) {
+                        for (let i of tweets){
+                                i.avatar = everyUser.find(user => user.username === i.username).avatar
+                                return tweets
+                                    }
+               } else {
+                        const lasts = tweets.slice(0,10);
+                for (let i of lasts){
+                        i.avatar = everyUser.find(user => user.username === i.username).avatar
+                        return lasts
+                        }
+                        }    
+         }
 }
-
 app.post("/sign-up", (req, res) => { 
-       everyUsers.push(req.body)
-       res.send("Ok")
+        const {username, avatar} = req.body
+        everyUser.push({
+               username,
+               avatar
+       })
+       res.send("OK")
 });
 
 app.post("/tweets", (req, res) => { 
-        tweets.unshift(req.body)
+        const {username, tweet} = req.body
+        tweets.unshift({
+                username,
+                tweet
+        })
         res.send("Ok")
  });
-
+ app.get("/tweets", (req, res) => { 
+        res.send(lastTweets())
+ });
 
 app.listen(5000);
